d8c18aee0ab56b66318e2246843ddc7ebe2f09b7
Amit <amitraheja291@gmail.com>
2024-03-01 15:14:12 +0530

    JoinNGetGroup

---
+++ Backend/VTrivia/Repository/IRepository/IAppUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Hosting;
+using VTrivia.Model;
+
+namespace VTrivia.Repository.IRepository
+{
+    public interface IAppUserRepository
+    {
+        IEnumerable<AppUser> GetAll();
+        AppUser Get(string id);
+
+        //IEnumerable<AppUser> GetAppUser(string id);
+        AppUser Add(AppUser entity);
+        void Remove(int id);
+
+        AppUser Update(AppUser entity);
+        void RemoveRange(IEnumerable<AppUser> entities);
+    }
+}
