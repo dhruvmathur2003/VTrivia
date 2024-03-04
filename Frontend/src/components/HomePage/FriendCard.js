e59f0dddbe8cacf74aa536106ddd5d98d3dedb09
Aryan Sanghi <aryansanghi008@gmail.com>
2024-03-05 00:40:20 +0530

    updating landing page

--- Frontend/src/components/HomePage/FriendCard.js
+++ Frontend/src/components/HomePage/FriendCard.js
@@ -3,7 +3,7 @@ import React from "react";
 const FriendCard = (props) => {
   return (
     <>
-      <div className="w-full max-w-sm bg-sky-400 border border-gray-800 rounded-lg shadow dark:bg-sky-400 dark:border-gray-700 pt-3">
+      <div className="w-full max-w-sm bg-sky-400 border border-gray-800 rounded-md shadow dark:bg-sky-400 dark:border-gray-700 pt-3">
         <div className="flex flex-col items-center pb-10">
           <img
             className="w-24 h-24 mb-3 rounded-full shadow-lg"
@@ -19,7 +19,7 @@ const FriendCard = (props) => {
           <div className="flex mt-4 md:mt-6">
             <a
               href={props.Linkedin}
-              className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-sky-700 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Linkedin
             </a>
